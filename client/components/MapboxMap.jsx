28bd2bb28536dcd900f8cfd9f64db3d43d4b8341
Toily <escusado@gmail.com>
2017-10-10 21:02:15 -0500

    adds config

--- client/components/MapboxMap.jsx
+++ client/components/MapboxMap.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import ReactMapboxGl, { ZoomControl, Source, Layer, Feature } from 'react-mapbox-gl';
-
-const RASTER_SOURCE_OPTIONS = {
-  'type': 'raster',
-  'tiles': [
-    '',
-  ],
-  'tileSize': 512
-};
+import ReactMapboxGl, { Marker, RotationControl, ScaleControl, ZoomControl, Source, Layer, Feature } from 'react-mapbox-gl';
+import config from '../../config.json';
 
 const Map = ReactMapboxGl({
-  accessToken: ''
+  accessToken: config['mapbox.api.key']
 });
 
 export default class MapboxMap extends React.Component {
@@ -25,16 +18,15 @@ export default class MapboxMap extends React.Component {
           height: '100vh',
           width: '100vw'
         }}>
-          <Layer
-            type='symbol'
-            id='marker'
-            layout={{ 'icon-image': 'marker-15' }}>
-            <Feature coordinates={[-95.10458156746033,16.564658628571426]}/>
-          </Layer>
+        <Marker
+          coordinates={[-95.10458156746033,16.564658628571426]}
+          anchor="bottom">
+          <img src={'https://i.imgur.com/9rCpWsN.png'}/>
+        </Marker>
 
-          <Source id='source_id' tileJsonSource={RASTER_SOURCE_OPTIONS} />
-          <Layer type='raster' id='layer_id' sourceId='source_id' />
+          <RotationControl/>
           <ZoomControl/>
+          <ScaleControl/>
 
       </Map>
     );
